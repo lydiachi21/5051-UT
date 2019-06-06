55aa3424d8b69aca29576d84e3903bf4d12191ad
Chia-Ling Chi <chichialing@seattleu.edu>
2019-06-06 15:57:31 -0700

    Added test case template for ReportViewModel_NumberOfUsers_Get_Default_Should_Pass()

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -18,5 +18,20 @@ namespace UnitTests.Models
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void ReportViewModel_NumberOfUsers_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+            int myTestNumUsers = 10;
+
+            // Act
+            myTest.NumberOfUsers = myTestNumUsers;
+            var result = myTest.NumberOfUsers;
+
+            // Assert
+            Assert.AreEqual(10, result);
+        }
     }
 }
