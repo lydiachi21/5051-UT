4cc01888abe4c90b16081216ce73e92694d4522f
Chia-Ling Chi <chichialing@seattleu.edu>
2019-06-06 15:12:16 -0700

    Added LogViewModelTests.CS to the project

---
+++ UnitTests/Models/LogViewModelTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
